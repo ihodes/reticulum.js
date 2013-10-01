0fb6b1629b5675294033a71ebc10b256ddfae7a6
Isaac Hodes <ihodes@mac.com>
2013-10-01 19:51:01 -0400

    FSM is now networked, stored in, in-memory 'DB' Can't construct fsm yet, can just reify one specified inline.

--- app.js
+++ app.js
@@ -17,14 +17,42 @@ var app = express();
 app.use(express.bodyParser());
 app.use(logging.requestLogger);
 
-
 // Routing
 app.all('/v1', function(req, res) {
     res.send({message: "You are connected to the API", status: 200});
 });
 
 app.namespace('/v1', function() {
-    app.get('/test', function(req, res) { res.send({hey:"world"})});
+    app.namespace('/fsm', function() {
+        app.post('/reify', function(req, res) { 
+            // for now, just return the reified fsm
+            var fsmM = r.reify(fsm, 'stateA');
+            fsmDb.push(fsmM);
+            res.send({success: true, fsmM: fsmM, id: fsmDb.length-1});
+        });
+        app.get('/:fsmid', function(req, res) { 
+            var id   = parseInt(req.params.fsmid),
+                fsmM = fsmDb[id];
+            if (fsmM)
+                res.send({fsmM: fsmM, id: id});
+            else
+                res.status(404).send({error: 404})
+        });
+        app.post('/:fsmid/send/:event', function(req, res) { 
+            var id    = parseInt(req.params.fsmid),
+                event = req.params.event,
+                args  = req.body.args,
+                fsmM  = fsmDb[id];
+
+            var fsmM1 = r.send(fsmM, event, args);
+            fsmDb[id] = fsmM1;
+
+            if (fsmM)
+                res.send({msg: 'msg recieved', event: event, args: args, fsmM: fsmM1});
+            else
+                res.status(404).send({error: 404})
+        });
+    })
 });
 
 app.all('*', function (req, res) {
@@ -36,3 +64,40 @@ app.listen(config.settings.PORT, function () {
     logger.info("Listening on port " + config.settings.PORT);
 });
 
+
+
+// db
+var fsmDb = [];
+
+
+// stubs
+var fsm = {
+    stateA: { 
+        actions: {
+            event: [
+                function (globals, evt, args) { if(evt === 'gotoB') return 'stateB'; },
+                function (globals, evt, args) { return 'stateA1'; }
+            ]
+        },
+
+        substates: {
+
+            stateA1: {
+                actions: {
+                    enter: [
+                        function (globals, evt, args) { globals.magic = 1; }
+                    ]
+                }
+            }
+
+        }
+    },
+
+    stateB: {
+        actions: {
+            event: [
+                function (globals, evt, args) { return 'stateA'; }
+            ]
+        }
+    }
+};
